8aad8ff9fb1473a6d85e8627196fdafdb2db82fa
Abdelkhalek <absarhan02@gmail.com>
2026-08-08 00:38:43 +0300

    Update Firebase configuration with hardcoded values
    Replaced Firebase configuration with hardcoded values.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD6XQJ-dki6ZFp5XZ-2xdmTXOcgCDo9YnQ",
+  authDomain: "presenze-scuola-af265.firebaseapp.com",
+  projectId: "presenze-scuola-af265",
+  storageBucket: "presenze-scuola-af265.firebasestorage.app",
+  messagingSenderId: "827792952584",
+  appId: "1:827792952584:web:f73726cb9f9a74a33ef0bd",
 };
 
-export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
